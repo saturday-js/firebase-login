7d3dc8bfa210fc6ae4b42edfb43dd3303b4bfb22
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2017-10-12 15:47:30 +0700

    add file

---
+++ src/pages/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+
+    <order-card @cardOnClick="goTo" v-for="(preOrder, key) in preOrders" :preOrder="preOrder" :key="key"></order-card>
+
+    <router-link to="/newpreorder" class=""><h1 class="title is-1">+ Pre Order</h1></router-link>
+    <br>
+
+    <a @click="logout"><h1 class="title is-1">Logout</h1></a>
+  </div>
+</template>
+
+<script>
+import OrderCard from './OrderCard'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'home',
+  components: {
+    OrderCard
+  },
+  computed: {
+    ...mapGetters([
+      'user',
+      'userProfile',
+      'contacts',
+      'preOrders'
+    ])
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    ...mapActions({
+      logout: 'logout'
+    }),
+    goTo (path) {
+      this.$router.push('/preorder/' + path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="login--container">
+    <div v-if="!isReady" class="">
+      Loading...
+    </div>
+    <button v-else class="button" @click="login()">login</button>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['isReady'])
+  },
+  methods: {
+    ...mapActions({
+      login: 'login'
+    })
+  }
+}
+</script>
+
+<style scoped>
+.login--container {
+  float: left;
+  width: 100%;
+  background-color: #fff;
+}
+</style>
